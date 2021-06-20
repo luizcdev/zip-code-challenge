4cf81b68012274ad7b46e72797f24affe8183e1e
Luiz <luiz.junior@luizalabs.com>
2021-06-20 03:27:40 -0300

    feat: onfiguring database access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseConfigModule } from './config/mongoose/mongoose.config.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MongooseConfigModule],
   controllers: [],
   providers: [],
 })
